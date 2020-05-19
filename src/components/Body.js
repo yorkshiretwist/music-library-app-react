a24c26372d0f7ebdf04e1fe17c93bd19cb60909c
Chris Taylor <chris@stillbreathing.co.uk>
2020-05-19 09:51:03 +0100

    Small update to test Github Actions

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import React from "react";
 const Body = () => <body>
     <h1>Welcome to the app</h1>
     <p>This is a simple music library app built with React</p>
+    <p>Small update to test Github Actions.</p>
     <script src="/main.js"></script>
 </body>;
 
